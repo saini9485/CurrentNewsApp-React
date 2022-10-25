1a0de1ed7ff90ad7db59a160b8b09f6d62d2bcab
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-10-25 22:07:11 +0530

    News App

--- src/components/CurrentNewsApp/DataButton.jsx
+++ src/components/CurrentNewsApp/DataButton.jsx
@@ -8,8 +8,22 @@ export function DataButton() {
   const [count, setCount] = useState(0);
   const [countD, setCountD] = useState(0);
   const [saved, setSaved] = useState("unSaved");
+  const [comment, setComment] = useState([]);
+  const [comments, setComments] = useState("");
+  const [commentCounter, setCommentCounter] = useState(0);
 
-  const CountLikes = () => {
+  const HandleComment = (e) => {
+    if (comments == "") {
+      alert("please Enter Comment");
+    } else {
+      setComment([...comment, comments]);
+      setComments("");
+    }
+    setCommentCounter(commentCounter + 1);
+  };
+
+  const CountLikes = (e) => {
+    e.preventDefault();
     setCount(count + 1);
   };
   const CountDisLikes = () => {
@@ -22,21 +36,54 @@ export function DataButton() {
   return (
     <div className="News-button_area">
       <div className="Reaction-Btn_Container">
-       <span> <i className="fas fa-heart fa-2x" onClick={CountLikes}></i>0</span>
+        <span>
+          {" "}
+          <i className="fas fa-heart fa-1.5x" onClick={CountLikes}>
+            {" "}
+          </i>
+          {count}
+        </span>
         {/* <span className="like-counter">0</span> */}
-       <span> <i className="fa fa-thumbs-down fa-2x" onClick={CountDisLikes}></i>0</span>
-         <span> <i className="fas fa-comment fa-2x" onClick={ShowComment}></i>0</span>
-         {/*<div className="Share_Btn">  <i className="fa-solid fa-share"></i></div>
-        <div className="BookMark-Btn">
-          <i className="fa-regular fa-bookmark" onClick={SavedUnSaved}></i>
-        </div> */}
+        <span>
+          {" "}
+          <i className="fa fa-thumbs-down fa-1.5x" onClick={CountDisLikes}></i>
+          {countD}
+        </span>
+        <span>
+          {" "}
+          <i className="fas fa-comment fa-1.5x" onClick={ShowComment}></i>
+          {commentCounter}
+        </span>
+         <i className="fa-solid fa-share " ></i>
       </div>
+      {/*<div className="Share_Btn">  <i className="fa-solid fa-share"></i></div>
+        // <div className="BookMark-Btn"> 
+        //   <i className="fa-regular fa-bookmark" onClick={SavedUnSaved}></i>
+        // </div> */}
       {/* <div className="LikeDislike_Count">
         <h5 className="Likes_Count">{count} Likes </h5>
         <h5 className="Dislike_Count"> {countD} DisLikes</h5>
         <h5 className="Dislike_Count"> Share</h5>
         <h5 className="SavedUnSaved">{saved}</h5>
       </div> */}
+
+   
+      <div className="post_comment">
+          <h5>userName</h5>
+          <p className="NewsComment">{comment}</p>
+      </div>
+      <div className="add-comment">
+        <input
+          type="text"
+          value={comments}
+          className="Input_Comment"
+          placeholder={"😒Add Comments...."}
+          onChange={(e) => setComments(e.target.value)}
+        ></input>
+        <button className="Comment_Btn" onClick={HandleComment}>
+          Comment
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/CurrentNewsApp/Headline.jsx
+++ src/components/CurrentNewsApp/Headline.jsx
@@ -7,8 +7,6 @@ export function Headline() {
   const [news, setNews] = useState([]);
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(false);
-  const [comment, setComment] = useState([]);
-  const [comments, setComments] = useState("");
 
   useEffect(() => {
     const LoadNews = async () => {
@@ -43,19 +41,11 @@ export function Headline() {
     const FilterData = news.filter((items) => items.url !== url);
     setNews(FilterData);
   };
-  const HandleComment = (e) => {
-    if (comments == "") {
-      alert("please Enter Comment");
-    } else {
-      setComment([...comment, comments]);
-      setComments("");
-    }
-  };
 
   return (
-    <div className="Main_Container">
-      {/* <h1 className="News-text">Top Business HeadLine News</h1>
-      <div className="News-search">
+    <>
+      <h1 className="News-text">Top Business HeadLine News</h1>
+      {/* <div className="News-search">
         <form className="Head_Input_Area">
           <input
             className="News-input"
@@ -66,78 +56,54 @@ export function Headline() {
           <button className="Search_Btn">Search</button>
         </form>
       </div> */}
-      {loading ? (
-        <h1 className="fa-refresh">
-          <i className="fa fa-refresh fa-spin"></i>
-        </h1>
-      ) : (
-        news
-          ?.filter((data) => filterFun(data))
-          .map((items) => {
-            return (
-              <div className="News_Cart">
-                <div className="News_Data" key={items.url}>
-                  <img className="Image" src={items.urlToImage} />
-                  <h2>name:{items.source.name}</h2>
-                  {/* <h2>author:{items.author.length>40?`${items.author.substring(0,40)}.`:items.author}</h2> */}
-                  <p className="Title"> title:{items.title} </p>
-                  <p>
-                    {items.description.length > 120
-                      ? `${items.description.substring(0, 120)}...`
-                      : items.description}
-                  </p>
-                  <div className="Publice_Time">
-                    <h5>Publish Time:</h5>
-                    <p>{items.publishedAt}</p>
-                  </div>
-                  <div className="Delete_Btn_Container">
-                    <button className="ReadMore_News_Btn">
-                      <a href={items.url} >
-                        Read More
-                      </a>
-                    </button>
-                    <button className="News_Remove-Btn"   key={items.url}
-                      onClick={() => handleRemove(items.url)}  >Remove</button>
-                  </div>
-                  {/* <div className="ReadMore-News_Btn">
-                   <button className="ReadMore_Btn"> <a href={items.url}>Read More...</a></button>
-                  </div>
-                  <div className="Delete_News_Btn">
-                    <button className="Delete_News">Delete</button>
-                    </div> */}
-                  <DataButton />
-
-                  <div className="like_icons">
-                    <div className="post_comment">
-                      <h5>userName</h5>
-                      <p className="NewsComment">{comment}</p>
+      <div className="Main_Container">
+        {loading ? (
+          <h1 className="fa-refresh">
+            <i className="fa fa-refresh fa-spin"></i>
+          </h1>
+        ) : (
+          news
+            ?.filter((data) => filterFun(data))
+            .map((items) => {
+              return (
+                <div className="News_Cart">
+                  <div className="News_Data" key={items.url}>
+                    <img className="Image" src={items.urlToImage} />
+                    <h2>name:{items.source.name}</h2>
+                    {/* <h2>author:{items.author.length>40?`${items.author.substring(0,40)}.`:items.author}</h2> */}
+                    <p className="Title"> title:{items.title} </p>
+                    <p>
+                      {items.description.length > 120
+                        ? `${items.description.substring(0, 120)}...`
+                        : items.description}
+                    </p>
+                    <div className="Publice_Time">
+                      <h5>Publish Time:</h5>
+                      <p>{items.publishedAt}</p>
                     </div>
+                    <div className="Delete_Btn_Container">
+                      <button className="ReadMore_News_Btn">
+                        <a href={items.url}>Read More</a>
+                      </button>
+                      <button
+                        className="News_Remove-Btn"
+                        key={items.url}
+                        onClick={() => handleRemove(items.url)}
+                      >
+                        Remove
+                      </button>
+                      {/* <i
+                        style={{ marginLeft: "270px", cursor: "pointer" }}
+                        className="fa-solid fa-share"
+                      ></i> */}
+                    </div>
+                    <DataButton />
                   </div>
-                  <div className="add-comment">
-                    <input
-                      type="text"
-                      value={comments}
-                      className="Input_Comment"
-                      placeholder={"😒Add Comments...."}
-                      onChange={(e) => setComments(e.target.value)}
-                    ></input>
-                    <button className="Comment_Btn" onClick={HandleComment}>
-                      Comment
-                    </button>
-                  </div>
-                  {/* <div className="News_Delete_Btn">
-                    <i
-                      class="fa fa-trash"
-                      aria-hidden="true"
-                      key={items.url}
-                      onClick={() => handleRemove(items.url)}
-                    ></i>
-                  </div> */}
                 </div>
-              </div>
-            );
-          })
-      )}
-    </div>
+              );
+            })
+        )}
+      </div>
+    </>
   );
 }
